4560d774002da156f8fcd4b749999c17d7e51fae
YuriiTrofimenko <tyaa@ukr.net>
2020-12-12 22:42:45 +0200

    firebase init

---
+++ src/configs/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC-dt3oYLAWDi9Mz_P-gyPZKR_QiT7jFpk",
+  authDomain: "public-home-library.firebaseapp.com",
+  projectId: "public-home-library",
+  storageBucket: "public-home-library.appspot.com",
+  messagingSenderId: "314828013684",
+  appId: "1:314828013684:web:a1cd575d9cabe1ec17d121",
+  measurementId: "G-285V68YWRB"
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+import { firebaseConfig } from './configs/firebaseConfig'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
